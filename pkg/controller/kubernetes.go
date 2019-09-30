44038f7d6421419c6778ae45aa208410ff104029
Jacob Vallejo <jakeev@amazon.com>
2019-09-30 15:36:34 -0700

    dogswatch: move to extras/

---
+++ pkg/controller/kubernetes.go
@@ -0,0 +1,70 @@
+package controller
+
+import (
+	"github.com/amazonlinux/thar/dogswatch/pkg/intent"
+	"github.com/amazonlinux/thar/dogswatch/pkg/k8sutil"
+	"github.com/pkg/errors"
+	"github.com/sirupsen/logrus"
+	v1meta "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/kubectl/pkg/drain"
+)
+
+func (am *ActionManager) cordonNode(nodeName string) error {
+	log := am.log.WithField("node", nodeName)
+	log.Debug("preparing to cordon")
+	node, err := am.kube.CoreV1().Nodes().Get(nodeName, v1meta.GetOptions{})
+	if err != nil {
+		return errors.WithMessage(err, "unable to retrieve node from api")
+	}
+	helper := drain.NewCordonHelper(node)
+	if helper.UpdateIfRequired(true) {
+		log.Debug("cordoning node")
+		err, patchErr := helper.PatchOrReplace(am.kube)
+		if err != nil {
+			return errors.WithMessage(err, "unable to submit node patch")
+		}
+		if patchErr != nil {
+			return errors.WithMessage(err, "unable to generate patch for node")
+		}
+		return nil
+	} else {
+		log.Debug("node is already cordoned")
+	}
+	return nil
+}
+
+func (am *ActionManager) drainWorkload(nodeName string) error {
+	log := am.log.WithField("node", nodeName)
+	log.Debug("draining workload")
+	helper := drain.Helper{
+		Client:              am.kube,
+		Out:                 am.log.WriterLevel(logrus.InfoLevel),
+		ErrOut:              am.log.WriterLevel(logrus.ErrorLevel),
+		IgnoreAllDaemonSets: true,
+	}
+	pods, errs := helper.GetPodsForDeletion(nodeName)
+	if len(errs) != 0 {
+		for _, e := range errs {
+			log.Error(e)
+		}
+		return errors.New("errors encountered while listing pods for deletion")
+	}
+	var err error
+	npods := len(pods.Pods())
+	if npods > 0 {
+		log.Debugf("%d pods present, removing workload", npods)
+		err = helper.DeleteOrEvictPods(pods.Pods())
+		if err == nil {
+			log.Debug("workload drained successfully")
+		}
+	} else {
+		log.Debug("no workload present")
+	}
+	return err
+}
+
+func (am *ActionManager) postIntent(i *intent.Intent) error {
+	log := am.log.WithField("node", i.NodeName)
+	defer log.Debugf("posted intent on node: %#v", i)
+	return k8sutil.PostMetadata(am.kube.CoreV1().Nodes(), i.NodeName, i)
+}
---
+++ pkg/platform/updog/updog.go
@@ -0,0 +1,105 @@
+package updog
+
+import (
+	"os"
+	"os/exec"
+	"path/filepath"
+
+	"github.com/amazonlinux/thar/dogswatch/pkg/thar"
+	"github.com/pkg/errors"
+)
+
+var (
+	updogBin = filepath.Join(thar.PlatformBin, "updog")
+)
+
+// updog implements the binding for the platform to the host's implementation
+// for manipulating updates on its behalf.
+type updog struct {
+	Bin command
+}
+
+type command interface {
+	CheckUpdate() (bool, error)
+	Update() error
+	UpdateImage() error
+	Reboot() error
+	Status() (bool, error)
+}
+
+type executable struct{}
+
+func (e *executable) runOk(cmd *exec.Cmd) (bool, error) {
+	cmd.SysProcAttr = thar.ProcessAttrs()
+	if err := cmd.Start(); err != nil {
+		return false, err
+	}
+	err := cmd.Wait()
+	return err == nil, err
+}
+
+func (e *executable) CheckUpdate() (bool, error) {
+	return e.runOk(exec.Command(updogBin, "check-update"))
+}
+
+func (e *executable) Update() error {
+	_, err := e.runOk(exec.Command(updogBin, "update"))
+	return err
+}
+
+func (e *executable) UpdateImage() error {
+	_, err := e.runOk(exec.Command(updogBin, "update-image"))
+	return err
+}
+
+func (e *executable) Reboot() error {
+	// TODO: reboot
+	_, err := e.runOk(exec.Command("reboot"))
+	return err
+}
+
+func (e *executable) Status() (bool, error) {
+	_, err := os.Stat(thar.RootFS + updogBin)
+	if err != nil {
+		return false, errors.Wrap(err, "updog not found in thar container mount "+thar.RootFS)
+	}
+	_, err = e.runOk(exec.Command(updogBin))
+	return err == nil, err
+}
+
+func newUpdogHost() Host {
+	return &updog{Bin: &executable{}}
+}
+
+func (u *updog) Status() (*statusResponse, error) {
+	if _, err := u.Bin.Status(); err != nil {
+		return nil, err
+	}
+	return &statusResponse{}, nil
+}
+
+func (u *updog) ListAvailable() (*listAvailableResponse, error) {
+	if _, err := u.Bin.CheckUpdate(); err != nil {
+		return nil, err
+	}
+	return &listAvailableResponse{}, nil
+}
+
+func (u *updog) PrepareUpdate(id UpdateID) (*prepareUpdateResponse, error) {
+	// TODO: extend updog for prepare steps
+	return &prepareUpdateResponse{}, nil
+}
+
+func (u *updog) ApplyUpdate(id UpdateID) (*applyUpdateResponse, error) {
+	if err := u.Bin.UpdateImage(); err != nil {
+		return nil, err
+	}
+	return &applyUpdateResponse{}, nil
+}
+
+func (u *updog) BootUpdate(id UpdateID, rebootNow bool) (*bootUpdateResponse, error) {
+	if err := u.Bin.Update(); err != nil {
+		return nil, err
+	}
+	return &bootUpdateResponse{}, nil
+}
