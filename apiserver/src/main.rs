6d71887c006ce166f6d1c845d1ccda3070c9c911
Sean P. Kelly <seankell@amazon.com>
2021-10-07 17:51:46 +0000

    Write apiserver failures messages to k8s termination log

--- apiserver/src/main.rs
+++ apiserver/src/main.rs
@@ -4,8 +4,28 @@ use models::node::K8SBottlerocketNodeClient;
 
 use snafu::ResultExt;
 
+use std::env;
+use std::fs;
+
+// By default, errors resulting in termination of the apiserver are written to this file,
+// which is the location kubernetes uses by default to surface termination-causing errors.
+const TERMINATION_LOG: &str = "/dev/termination-log";
+
 #[actix_web::main]
-async fn main() -> Result<()> {
+async fn main() {
+    let termination_log = env::var("TERMINATION_LOG")
+        .map_err(|_| TERMINATION_LOG.to_string())
+        .unwrap();
+    match run_server().await {
+        Err(error) => {
+            fs::write(&termination_log, format!("{}", error))
+                .expect("Could not write k8s termination log.");
+        }
+        Ok(()) => {}
+    }
+}
+
+async fn run_server() -> Result<()> {
     env_logger::init();
 
     let k8s_client = kube::client::Client::try_default()
