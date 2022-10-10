5e6251fb69cb9d814d32dd9f9dca63b4b592a6f9
Tianhao Geng <tianhg@amazon.com>
2022-10-10 17:46:26 +0000

    Reload certificate periodically
    when cert-manager rotates the certificate brupop-apiserver-certificate
    (which by default will be after 60 days), brupop apiserver needs to reload
    certificate to bounce all the agent and APIServer pods.

--- models/src/node/mod.rs
+++ models/src/node/mod.rs
@@ -1,14 +1,17 @@
 mod client;
 mod crd;
 mod drain;
-mod error;
+pub mod error;
 
 pub use self::client::*;
 pub use self::crd::*;
 pub use self::error::Error as BottlerocketShadowError;
+use error::Result;
 
 use lazy_static::lazy_static;
 pub use semver::Version;
+use snafu::ResultExt;
+use std::io::Read;
 
 lazy_static! {
     // Regex gathered from semver.org as the recommended semver validation regex.
@@ -29,3 +32,16 @@ pub const K8S_NODE_KIND: &str = "BottlerocketShadow";
 pub const K8S_NODE_PLURAL: &str = "bottlerocketshadows";
 pub const K8S_NODE_STATUS: &str = "bottlerocketshadows/status";
 pub const K8S_NODE_SHORTNAME: &str = "brs";
+
+pub fn read_certificate(public_key_path: &str) -> Result<Vec<u8>> {
+    let mut buf = Vec::new();
+
+    std::fs::File::open(public_key_path)
+        .map_err(|err| Box::new(err) as Box<dyn std::error::Error>)
+        .context(error::IOSnafu)?
+        .read_to_end(&mut buf)
+        .map_err(|err| Box::new(err) as Box<dyn std::error::Error>)
+        .context(error::IOSnafu)?;
+
+    Ok(buf)
+}
